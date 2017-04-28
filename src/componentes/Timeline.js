79de7e9b0929477aeb393c81a2224a7d009915ea
Rangel <rangel.varnier@gmail.com>
2017-04-27 23:48:38 -0300

    refatoração presentational component

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -14,11 +14,11 @@ export default class Timeline extends Component {
         }
     }
 
-    carregaFotos(){
-        const urlPerfil = !!this.login 
-        ? `http://localhost:8080/api/public/fotos/${this.login}` 
-        : `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`
-        
+    carregaFotos() {
+        const urlPerfil = !!this.login
+            ? `http://localhost:8080/api/public/fotos/${this.login}`
+            : `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`
+
         fetch(urlPerfil)
             .then(retorno => retorno.json())
             .then(fotos => {
@@ -26,45 +26,67 @@ export default class Timeline extends Component {
             })
     }
 
-    like(fotoId){
-        fetch(`http://localhost:8080/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, {method: 'POST'})
-        .then(response => {
-            if(response.ok) {
+    like(fotoId) {
+        fetch(`http://localhost:8080/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, {method: 'POST'}).then(response => {
+            if (response.ok) {
                 return response.json();
-            }else{
+            } else {
                 throw new Error("não foi possivel realizar like na foto");
             }
-        })
-        .then(liker => {
+        }).then(liker => {
             Pubsub.publish('atualiza-liker', {fotoId, liker});
         })
     }
 
-    comenta(fotoId, comentario){
+    comenta(fotoId, comentario) {
         const requestInfo = {
-        method: 'POST',
-        body : JSON.stringify({texto: comentario}),
-        headers : new Headers({
-            'Content-type' : 'application/json'
-        })
+            method: 'POST',
+            body: JSON.stringify({texto: comentario}),
+            headers: new Headers({'Content-type': 'application/json'})
         }
 
-        fetch(`http://localhost:8080/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
-        .then(response => {
-            if(response.ok){
-            return response.json();
-            }else{
-            throw new Error("não foi possivel comentar");
+        fetch(`http://localhost:8080/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo).then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("não foi possivel comentar");
             }
-        })
-        .then(novoComentario => {
+        }).then(novoComentario => {
             Pubsub.publish("novos-comentarios", {fotoId, novoComentario});
         })
     }
 
-    componentWillMount(){
+    componentWillMount() {
         Pubsub.subscribe('timeline', (topico, fotos) => {
             this.setState({fotos});
+        });
+
+        Pubsub.subscribe('atualiza-liker', (topico, infoLiker) => {
+            const fotoAchada = this.state.fotos
+                .find(foto => foto.id === infoLiker.fotoId);
+            fotoAchada.likeada = !fotoAchada.likeada;
+            const possivelLiker = fotoAchada.likers
+                .find(liker => liker.login === infoLiker.liker.login);
+
+            if (possivelLiker === undefined) {
+                fotoAchada.likers.push(infoLiker.liker);
+            } else {
+                const novosLikers = fotoAchada.likers
+                    .filter(liker => liker.login !== infoLiker.liker.login);
+                fotoAchada.likers = novosLikers;
+            }
+            this.setState({fotos: this.state.fotos});
+
+        });
+
+        Pubsub.subscribe('novos-comentarios', (topico, infoComentario) => {
+            const fotoAchada = this.state.fotos
+                .find(foto => foto.id === infoComentario.fotoId);
+
+            fotoAchada.comentarios
+                .push(infoComentario.novoComentario);
+
+            this.setState({fotos: this.state.fotos});
         });
     }
 
@@ -72,8 +94,8 @@ export default class Timeline extends Component {
         this.carregaFotos();
     }
 
-    componentWillReceiveProps(nextProps){
-        if(!!nextProps.login){
+    componentWillReceiveProps(nextProps) {
+        if (!!nextProps.login) {
             this.login = nextProps.login;
             this.carregaFotos();
         }
@@ -86,7 +108,10 @@ export default class Timeline extends Component {
                     transitionName="timeline"
                     transitionEnterTimeout={500}
                     transitionLeaveTimeout={300}>
-                    {this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like} comenta={this.comenta}/>)}
+                    {this
+                        .state
+                        .fotos
+                        .map(foto => <FotoItem key={foto.id} foto={foto} like={this.like} comenta={this.comenta}/>)}
                 </ReactCSSTransitionGroup>
             </div>
         );

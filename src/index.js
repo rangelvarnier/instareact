52197529b37e51083cecda8854b563b4dfefd7e6
Rangel <rangel.varnier@gmail.com>
2017-05-10 00:41:58 -0300

    adicionado react-redux para fazer uso do Provider, que será responsavel de repassar a store (unica) para todos componentes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import Login from './componentes/login/Login';
 import Logout from './componentes/logout/Logout';
 import {Router, Route, browserHistory} from 'react-router';
 import {matchPattern} from 'react-router/lib/PatternUtils';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {timeline} from './reducers/timeline';
+import {notificacao} from './reducers/header';
+import {Provider} from 'react-redux';
 
 function verificaAutenticacao(nextState, replace) {
   const resultado = matchPattern('/timeline(/:login)', nextState.location.pathname);
@@ -15,13 +20,18 @@ function verificaAutenticacao(nextState, replace) {
   }
 }
 
+const reducers = combineReducers({timeline, notificacao});
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={Login}/>
-    <Route path="/logout" component={Logout}/>
-    <Route
-      path="/timeline(/:login)"
-      component={App}
-      onEnter={verificaAutenticacao}/>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Login}/>
+      <Route path="/logout" component={Logout}/>
+      <Route
+        path="/timeline(/:login)"
+        component={App}
+        onEnter={verificaAutenticacao}/>
+    </Router>
+  </Provider>
 ), document.getElementById('root'));
